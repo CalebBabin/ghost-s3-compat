ff73dbee0c159f756b0c684032e9da8de1fd12ba
Caleb <CalebBabin@users.noreply.github.com>
2019-09-29 21:21:49 -0500

    switch to class based layout for ghost 2.x
    split options into seperate file
    
    ignore changes to options.js

--- index.js
+++ index.js
@@ -6,31 +6,17 @@ var path = require('path');
 var Bluebird = require('bluebird');
 var AWS = require('aws-sdk-promise');
 var readFileAsync = Bluebird.promisify(fs.readFile);
-var options = {};
-var util = require('util');
-var BaseStore;
-try {
-    BaseStore = require('ghost/core/server/storage/base');
-} catch (e) {
-    if (e.code !== 'MODULE_NOT_FOUND') throw e;
-    BaseStore = require(path.join(process.cwd(), 'core/server/storage/base'));
-}
 
-function S3Store(config) {
-    BaseStore.call(this);
-    options = config;
-}
+const options = require('./options.js');
+const BaseAdapter = require('ghost-storage-base');
 
-util.inherits(S3Store, BaseStore);
-
- /**
+/**
  * Return the URL where image assets can be read.
  * @param  {String} bucket [AWS S3 bucket name]
  * @return {String}        [path-style URL of the S3 bucket]
  */
 function getAwsPath(bucket) {
-    var awsRegion = (options.region == 'us-east-1') ? 's3' : 's3-' + options.region;
-    var awsPath = options.assetHost ? options.assetHost : 'https://' + awsRegion + '.amazonaws.com/' + options.bucket + '/';
+    var awsPath = options.assetHost ? options.assetHost : `https://${options.bucket}.s3.${options.region}.amazonaws.com/`;
     return awsPath;
 }
 
@@ -46,7 +32,7 @@ function getTargetName(image, targetDir) {
     var ext = path.extname(image.name);
     var name = path.basename(image.name, ext).replace(/\W/g, '_');
 
-    return targetDir + name + '-' + Date.now() + ext;
+    return targetDir.replace(/\\/g, '/') + name + '-' + Date.now() + ext;
 }
 
 function validOptions(opts) {
@@ -55,83 +41,86 @@ function validOptions(opts) {
         opts.bucket &&
         opts.region);
 }
-
-S3Store.prototype.save = function save(image) {
-    if (!validOptions(options)) {
-      return Bluebird.reject('ghost-s3 is not configured');
+class S3Store extends BaseAdapter {
+    constructor () {
+        super();
+    }
+    save(image) {
+        if (!validOptions(options)) {
+            return Bluebird.reject('ghost-s3 is not configured');
+        }
+    
+        var targetDir = this.getTargetDir();
+        var targetFilename = getTargetName(image, targetDir);
+    
+        var s3 = new AWS.S3({
+            accessKeyId: options.accessKeyId,
+            secretAccessKey: options.secretAccessKey,
+            bucket: options.bucket,
+            region: options.region,
+            s3ForcePathStyle: true,
+        });
+    
+        return readFileAsync(image.path)
+            .then(function(buffer) {
+                var params = {
+                    ACL: 'public-read',
+                    Bucket: options.bucket,
+                    Key: targetFilename,
+                    Body: buffer,
+                    ContentType: image.type,
+                    CacheControl: 'max-age=' + (1000 * 365 * 24 * 60 * 60) // 365 days
+                };
+    
+                return s3.putObject(params).promise();
+            })
+            .tap(function() {
+                logInfo('ghost-s3', 'Temp uploaded file path: ' + image.path);
+            })
+            .then(function(results) {
+                var awsPath = getAwsPath(options.bucket);
+                return Bluebird.resolve(awsPath + targetFilename);
+            })
+            .catch(function(err) {
+                logError(err);
+                throw err;
+            });
     }
 
-    var targetDir = this.getTargetDir();
-    var targetFilename = getTargetName(image, targetDir);
-
-    var s3 = new AWS.S3({
-        accessKeyId: options.accessKeyId,
-        secretAccessKey: options.secretAccessKey,
-        bucket: options.bucket,
-        region: options.region
-    });
-
-    return readFileAsync(image.path)
-        .then(function(buffer) {
-            var params = {
-                ACL: 'public-read',
-                Bucket: options.bucket,
-                Key: targetFilename,
-                Body: buffer,
-                ContentType: image.type,
-                CacheControl: 'max-age=' + (1000 * 365 * 24 * 60 * 60) // 365 days
-            };
-
-            return s3.putObject(params).promise();
-        })
-        .tap(function() {
-            logInfo('ghost-s3', 'Temp uploaded file path: ' + image.path);
-        })
-        .then(function(results) {
-            var awsPath = getAwsPath(options.bucket);
-            return Bluebird.resolve(awsPath + targetFilename);
-        })
-        .catch(function(err) {
-            logError(err);
-            throw err;
+    serve() {
+        var s3 = new AWS.S3({
+            accessKeyId: options.accessKeyId,
+            secretAccessKey: options.secretAccessKey,
+            bucket: options.bucket,
+            region: options.region,
+            s3ForcePathStyle: true,
         });
-};
-
-// middleware for serving the files
-S3Store.prototype.serve = function serve() {
-    var s3 = new AWS.S3({
-        accessKeyId: options.accessKeyId,
-        secretAccessKey: options.secretAccessKey,
-        bucket: options.bucket,
-        region: options.region
-    });
-
-    return function (req, res, next) {
-        var params = {
-            Bucket: options.bucket,
-            Key: req.path.replace(/^\//, '')
+    
+        return function (req, res, next) {
+            var params = {
+                Bucket: options.bucket,
+                Key: req.path.replace(/^\//, '')
+            };
+    
+            s3.getObject(params)
+                .on('httpHeaders', function(statusCode, headers, response) {
+                    res.set(headers);
+                })
+                .createReadStream()
+                .on('error', function(err) {
+                    logError(err);
+                    res.status(404);
+                    next();
+                })
+                .pipe(res);
         };
+    }
 
-        s3.getObject(params)
-            .on('httpHeaders', function(statusCode, headers, response) {
-                res.set(headers);
-            })
-            .createReadStream()
-            .on('error', function(err) {
-                logError(err);
-                res.status(404);
-                next();
-            })
-            .pipe(res);
-    };
-};
-
-// noop because Ghost requires this function to be defined
-// TODO: implement
-S3Store.prototype.exists = function exists() {};
-
-// noop because Ghost requires this function to be defined
-// TODO: implement
-S3Store.prototype.delete = function deleteFile() {};
+    // noop because Ghost requires this function to be defined
+    // TODO: implement
+    exists() {}
+    read() {}
+    delete() {}
+}
 
 module.exports = S3Store;
---
+++ options.js
@@ -0,0 +1,6 @@
+module.exports = {
+    accessKeyId: "<insert yours here>",
+    secretAccessKey: "<insert yours here>",
+    bucket: "<insert yours here>",
+    region: "<insert yours here>"
+};
